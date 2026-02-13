b072da255a488a1ee866bac1057fe6f194b44763
bbvv77 <mvc@li.cm>
2026-02-14 05:25:02 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -22,11 +22,7 @@ const SITE_CONFIG = {
 
 // API站点配置
 const API_SITES = {
-        testSource: {
-        api: 'https://www.example.com/api.php/provide/vod',
-        name: '空内容测试源',
-        adult: true
-    }
+
 };
 
 // 定义合并方法
